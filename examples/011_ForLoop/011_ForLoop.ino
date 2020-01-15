029188d6d1b8bf2ed5aa88d44d62285c455efbfd
juxtapix <juxtapix@macumba-4.local>
2020-01-15 07:51:18 -0500

    New IMGs

--- examples/011_ForLoop/011_ForLoop.ino
+++ examples/011_ForLoop/011_ForLoop.ino
@@ -1,17 +1,22 @@
-// ICP Workshop 2020
-// Digital Output - ForLoop
-
 void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(9600);       // initialize serial (console) at 9600 bps.
+  pinMode(5, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  for (int i = 0; i < 100; i++) {
+  // put your main code here, to run forever!!!!!!!!:
+  for (int i = 0; i <= 255; i++) {
     Serial.println( i );            // print to console
-    delay(100);                    // delay in between readability
+    analogWrite(5, i);
+    delay(10);                      // delay in between readability
   }
+
+  for (int i = 255; i >= 0; i--) {
+    Serial.println( i );            // print to console
+    analogWrite(5, i);
+    delay(10);                      // delay in between readability
+  }
+
 }
--- examples/012_Fade/012_Fade.ino
+++ examples/012_Fade/012_Fade.ino
@@ -1,27 +1,38 @@
 // ICP Workshop 2020
 // Digital Output - Fade
 
-const int ledPin = 10;
+// Constant:
+const int rPin = 5;
+const int gPin = 3;
+const int bPin = 2;
 const int fadeAmount = 5;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
+  pinMode(rPin, OUTPUT);          // initialize pwm pin as an output.
+  pinMode(gPin, OUTPUT);          // initialize pwm pin as an output.
+  pinMode(bPin, OUTPUT);          // initialize pwm pin as an output.
 }
 
 void loop() {
   // fade in from min to max in increments of 5 points:
   for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += fadeAmount) {
     // sets the value (range from 0 to 255):
-    analogWrite(ledPin, fadeValue);
+    rgb(0,0,fadeValue);
     // wait for 30 milliseconds to see the dimming effect
-    delay(30);
+    delay(100);
   }
 
   // fade out from max to min in increments of 5 points:
   for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= fadeAmount) {
     // sets the value (range from 0 to 255):
-    analogWrite(ledPin, fadeValue);
+    rgb(0,0,fadeValue);
     // wait for 30 milliseconds to see the dimming effect
-    delay(30);
+    delay(100);
   }
+}
+
+void rgb(int r, int g, int b){
+  analogWrite(rPin, r);
+  analogWrite(gPin, g);
+  analogWrite(bPin, b);
 }
